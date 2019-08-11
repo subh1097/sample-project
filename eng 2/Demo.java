ff6bca37c9a159b8a5cdde188e761158fc847547
Subhendu <subhenduroy031@gmail.com>
2019-08-11 22:28:15 +0530

    Added Files

---
+++ eng 2/Demo.java
@@ -0,0 +1,10 @@
+class demo
+{
+
+public static void main(string[]args)
+{
+print ("hi ")
+print("Hello world")
+
+}
+}
---
+++ eng 2/Demo.py
@@ -0,0 +1,4 @@
+string="Hello world"
+string1="Hello"
+print (string)
+print(string1)
